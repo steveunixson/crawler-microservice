1d61291a53415a985b1c62add8b7eec6ad5c29b0
Steve Unixson <unixson@gmail.com>
2018-10-17 00:17:28 +0300

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const formData = require('express-form-data');
+const morgan = require('morgan');
+const os = require('os');
+const log = require('./utils/log')(module);
+const config = require('./config/config');
+const crawler = require('./routes/crawler');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const options = {
+  uploadDir: os.tmpdir(),
+  autoClean: false,
+};
+
+app.use(formData.parse(options));
+// clear from the request and delete all empty files (size == 0)
+app.use(formData.format());
+// change file objects to stream.Readable
+app.use(formData.stream());
+// union body and files
+app.use(formData.union());
+app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(crawler);
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port, () => {
+  log.info(config.colors.FgMagenta, `Bonobo 2gis gather now Running On : ${port}`);
+});
---
+++ routes/crawler.js
@@ -0,0 +1,172 @@
+const express = require('express');
+const os = require('os');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const ps = require('ps-node');
+const nodemailer = require('nodemailer');
+const log = require('../utils/log')(module);
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  requireTLS: true,
+  auth: {
+    user: 'commonbonobo@gmail.com',
+    pass: 'seb092qb',
+  },
+});
+
+const router = express.Router();
+/* GET users listing. */
+router.post('/crawl', (req, res) => {
+  const search = { query: req.body.crawlTask };
+  const url = { targetUrl: req.body.searchUrl };
+  const email = { targetEmail: req.body.sendTo };
+  if (req.body.searchUrl === undefined || req.body.crawlTask === undefined || req.body.sendTo === undefined) {
+    return res.status(400).json({ err: 1, msg: 'Bad request', crawlList: { search, url, email } });
+  }
+  ps.lookup({
+    command: 'chromium',
+    arguments: '--headless',
+  }, (err, resultList) => {
+    if (err) {
+      throw new Error(err);
+    }
+    resultList.forEach((process) => {
+      if (process) {
+        console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+      }
+    });
+    return res.status(200).json({
+      err: 0, msg: 'Crawl task has been started', crawlList: { search, url }, process: { resultList },
+    });
+  });
+  const formSelector = 'div#module-1-3-1 > div > input';
+  const submitButton = '#module-1-3 > div.searchBar__forms > div > form > button.searchBar__submit._directory';
+  const width = 1920;
+  const height = 1050;
+
+  const scrape = async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        `--window-size=${width},${height}`,
+      ],
+    });
+    const timeout = 4 * 30000;
+    const page = await browser.newPage();
+    await page.setViewport({ width, height });
+    await page.setDefaultNavigationTimeout(timeout);
+    try {
+      await log.info(`Crawler started with url: ${url.targetUrl}`);
+      await page.goto(url.targetUrl);
+    } catch (exception) {
+      log.error(`Exception caught: ${exception}`);
+    }
+    try {
+      log.info(`Search query: ${search.query}`);
+      await page.$eval(formSelector, (el, value) => el.value = value, search.query);
+    } catch (exception) {
+      log.error(`Exception caught: ${exception}`);
+    }
+    await page.click(submitButton);
+    await log.info(`clicked on ${submitButton}`);
+    await page.waitFor(5000);
+    const items = [];
+
+    try {
+      await page.waitForSelector('div.pagination__arrow._right');
+    } catch (exception) {
+      log.error(`EXCEPTION CAUGHT: ${exception}`);
+    }
+    try {
+      while (await page.$('div.pagination__arrow._right') !== null) {
+        await page.waitForSelector('.miniCard');
+        for (let step = 0; step < 12; step++) {
+          const action = await page.evaluate(() => {
+            const anchors = document.getElementsByClassName('miniCard__headerTitleLink');
+            return [].map.call(anchors, a => a.href);
+          });
+          const pageNew = await browser.newPage();
+          await pageNew.setViewport({ width, height });
+          await pageNew.setDefaultNavigationTimeout(timeout);
+          await pageNew.goto(action[step]);
+          try {
+            await pageNew.waitFor(1000);
+            await pageNew.waitForSelector('div.card__scrollerIn');
+            await pageNew.waitForSelector('h1.cardHeader__headerNameText');
+            await pageNew.waitForSelector('address.card__address');
+            await pageNew.waitForSelector('div.contact__toggle._place_phones');
+            await pageNew.evaluate(() => document.getElementsByClassName('contact__toggle _place_phones')[0].click());
+            const number = await pageNew.evaluate(() => document.getElementsByClassName('contact__phones _shown')[0].innerText);
+            const phone = number.replace(/(\r\n\t|\n|\r\t)/gm, ' ', ' ').split('Пожалуйста, скажите, что узнали номер в 2ГИС').join('');
+            const name = await pageNew.evaluate(() => document.querySelector('h1.cardHeader__headerNameText').innerText);
+            const Address = await pageNew.evaluate(() => document.querySelector('address.card__address').innerText);
+            items.push({
+              phoneNumber: phone,
+              companyName: name,
+              address: Address,
+            });
+            await pageNew.close();
+            await log.info(`Company collected: ${name}`);
+          } catch (e) {
+            await log.error(`Exception caught: ${e}`);
+            await pageNew.close();
+          }
+        }
+        await page.waitForSelector('div.pagination__arrow._right');
+        await page.focus('div.pagination__arrow._right');
+        await page.click('div.pagination__arrow._right');
+        await log.info('clicked on next page');
+      }
+    } catch (exception) {
+      log.error(`EXCEPTION CAUGHT: ${exception}`);
+    }
+    await browser.close();
+    return items;
+  };
+  scrape().then((value) => {
+    const content = JSON.stringify(value);
+    const homeDir = os.tmpdir();
+    const milliseconds = new Date().getTime();
+
+    try {
+      fs.writeFile(`${homeDir}/${search.query}_${milliseconds}.json`, content, 'utf8', (err) => {
+        if (err) {
+          return log.error(err);
+        }
+        log.info(`JSON file was created at: ${homeDir}/${search.query}_${milliseconds}.json | Sending Email`);
+        const mailOptions = {
+          from: 'commonbonobo@gmail.com',
+          to: email.targetEmail,
+          subject: 'Crawl result JSON',
+          text: `New Base Collected for ${search.query} at ${url.targetUrl}`,
+          attachments: [
+            {
+              filename: `${search.query}_${milliseconds}.json`,
+              path: `${homeDir}/${search.query}_${milliseconds}.json`,
+              contentType: 'application/json',
+            },
+          ],
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            log.error(error);
+          } else {
+            log.info(`Email sent: ${info.response}`);
+            transporter.close();
+          }
+        });
+        return 0;
+      });
+    } catch (e) {
+      log.error(`EXCEPTION CAUGHT: ${e}`);
+    }
+  }).catch((exception) => {
+    log.error(`EXCEPTION CAUGHT: ${exception}`);
+  });
+  return 0;
+});
+
+module.exports = router;
