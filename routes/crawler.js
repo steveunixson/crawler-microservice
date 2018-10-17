d859df1179ce6324f375f55628f21346c54ae3e9
Steve Unixson <unixson@gmail.com>
2018-10-17 17:04:05 +0300

    added propper screenshots and removed urls from json

--- routes/crawler.js
+++ routes/crawler.js
@@ -23,6 +23,7 @@ router.post('/crawl', (req, res) => {
   const search = { query: req.body.crawlTask };
   const url = { targetUrl: req.body.searchUrl };
   const email = { targetEmail: req.body.sendTo };
+  const milliseconds = new Date().getTime();
   if (req.body.searchUrl === undefined || req.body.crawlTask === undefined || req.body.sendTo === undefined) {
     return res.status(400).json({ err: 1, msg: 'Bad request', crawlList: { search, url, email } });
   }
@@ -39,13 +40,24 @@ router.post('/crawl', (req, res) => {
       }
     });
     return res.status(200).json({
-      err: 0, msg: 'Crawl task has been started', crawlList: { search, url }, process: { resultList },
+      err: 0,
+      msg: 'Crawl task has been started',
+      crawlList: { search, url },
+      unixTimeID: milliseconds,
+      process: { resultList },
     });
   });
   const formSelector = 'div#module-1-3-1 > div > input';
   const submitButton = '#module-1-3 > div.searchBar__forms > div > form > button.searchBar__submit._directory';
   const width = 1920;
   const height = 1050;
+  const homeDir = os.tmpdir();
+  const crawlerDir = '2gis-webgatherer';
+
+  if (!fs.existsSync(`${homeDir}/${crawlerDir}/`)) {
+    fs.mkdirSync(`${homeDir}/${crawlerDir}/`);
+    log.info(`Created directory: ${homeDir}/${crawlerDir}/`);
+  }
 
   const scrape = async () => {
     const browser = await puppeteer.launch({
@@ -103,24 +115,32 @@ router.post('/crawl', (req, res) => {
             const phone = number.replace(/(\r\n\t|\n|\r\t)/gm, ' ', ' ').split('Пожалуйста, скажите, что узнали номер в 2ГИС').join('');
             const name = await pageNew.evaluate(() => document.querySelector('h1.cardHeader__headerNameText').innerText);
             const Address = await pageNew.evaluate(() => document.querySelector('address.card__address').innerText);
-            const url = await page.url();
+            const Url = { url: pageNew.url() };
+            const unixTime = new Date().getTime();
+            await pageNew.screenshot({ path: `${homeDir}/${crawlerDir}/auto_${unixTime}.screenshot.jpeg`, type: 'jpeg', quality: 50 });
             items.push({
               phoneNumber: phone,
               companyName: name,
               address: Address,
-              pageUrl: url,
             });
             await pageNew.close();
-            await log.info(`Company collected: ${name}`);
+            await log.info(`Company collected: ${name}| url: ${Url.url}`);
           } catch (e) {
-            await log.error(`Exception caught: ${e}`);
+            const Url = { url: pageNew.url() };
+            const unixTime = new Date().getTime();
+            await pageNew.screenshot({ path: `${homeDir}/${crawlerDir}/auto_exception.${unixTime}.screenshot.jpeg`, type: 'jpeg', quality: 50 });
+            await log.error(`Exception caught at url ${Url.url}: ${e}`);
             await pageNew.close();
           }
         }
         await page.waitForSelector('div.pagination__arrow._right');
+        const pageIndex = await page.evaluate(() => document.querySelector('.pagination__page._current').innerText);
+        if (await page.evaluate(() => document.querySelector('div.pagination__arrow._right').className) === 'pagination__arrow _right _disabled'){
+          page.close();
+        }
         await page.focus('div.pagination__arrow._right');
         await page.click('div.pagination__arrow._right');
-        await log.info('clicked on next page');
+        await log.info(`clicked on page: ${pageIndex}`);
       }
     } catch (exception) {
       log.error(`EXCEPTION CAUGHT: ${exception}`);
@@ -130,15 +150,12 @@ router.post('/crawl', (req, res) => {
   };
   scrape().then((value) => {
     const content = JSON.stringify(value);
-    const homeDir = os.tmpdir();
-    const milliseconds = new Date().getTime();
-
     try {
-      fs.writeFile(`${homeDir}/${search.query}_${milliseconds}.json`, content, 'utf8', (err) => {
+      fs.writeFile(`${homeDir}/${crawlerDir}/${search.query}_${milliseconds}.json`, content, 'utf8', (err) => {
         if (err) {
           return log.error(err);
         }
-        log.info(`JSON file was created at: ${homeDir}/${search.query}_${milliseconds}.json | Sending Email`);
+        log.info(`JSON file was created at: ${homeDir}/${crawlerDir}/${search.query}_${milliseconds}.json | Sending Email`);
         const mailOptions = {
           from: 'commonbonobo@gmail.com',
           to: email.targetEmail,
@@ -147,7 +164,7 @@ router.post('/crawl', (req, res) => {
           attachments: [
             {
               filename: `${search.query}_${milliseconds}.json`,
-              path: `${homeDir}/${search.query}_${milliseconds}.json`,
+              path: `${homeDir}/${crawlerDir}/${search.query}_${milliseconds}.json`,
               contentType: 'application/json',
             },
           ],
