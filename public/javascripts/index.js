95f91f26d73d4ecb4ef1cf81a5ee9618b45cf833
Steve Unixson <unixson@gmail.com>
2018-10-26 01:17:03 +0300

    added web interface for youla

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,3 +12,17 @@ function postUser() {
       console.log(error);
     });
 }
+
+function postYoula() {
+  axios.post('/youla-crawl',  {
+    crawlTask: document.getElementById('inputYoula').innerText,
+    sendTo: document.getElementById('email-input-youla').value,
+    searchUrl: document.getElementById('inputYoula').value,
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
